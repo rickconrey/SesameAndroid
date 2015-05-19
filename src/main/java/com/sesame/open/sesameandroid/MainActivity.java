882c6fd3bb09bee0e11c9eaa975fc71f3c0bcbc8
Rick Conrey <rickconrey@gmail.com>
2015-05-19 16:40:47 -0700

    Initial bluetooth connection code, added listview to layout

--- src/main/java/com/sesame/open/sesameandroid/MainActivity.java
+++ src/main/java/com/sesame/open/sesameandroid/MainActivity.java
@@ -1,30 +1,32 @@
 package com.sesame.open.sesameandroid;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private final static int REQUEST_ENABLE_BT = 1;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
@@ -36,8 +38,40 @@ public class MainActivity extends ActionBarActivity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
+        runSesame(mBluetoothAdapter);
     }
 
+    public void runSesame(BluetoothAdapter mBluetoothAdapter) {
+        final UUID SSP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+        String address = "00:06:66:52:3C:88";
+        Set<BluetoothDevice> pairedDevices;
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
+        listView = (ListView)findViewById(R.id.listBTDevices);
+        ArrayList<String> deviceList = new ArrayList();
+        for (BluetoothDevice bt : pairedDevices) {
+            deviceList.add(bt.getName());
+        }
+        final ArrayAdapter arrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, deviceList);
+
+        listView.setAdapter(arrayAdapter);
+
+        BluetoothDevice btDevice = mBluetoothAdapter.getRemoteDevice(address);
+        mBluetoothAdapter.cancelDiscovery();
+        BluetoothSocket btSocket;
+
+        try {
+            btSocket = btDevice.createRfcommSocketToServiceRecord(SSP_UUID);
+            btSocket.connect();
+        } catch (IOException e) {
+            CharSequence text = "Error connecting to BT";
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,21 +93,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 }
